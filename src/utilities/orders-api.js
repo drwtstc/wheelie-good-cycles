5fcb61d612fb740db0c1f2f14aaeaa90dbe99a2a
drwtstc-ga <drew1886@me.com>
2023-02-15 17:09:25 -0500

    Products, Orders and send-request utilities complete

--- src/utilities/orders-api.js
+++ src/utilities/orders-api.js
@@ -1,14 +1,23 @@
-export function setItemQtyInCart(itemId, newQty) {
-    return(
-       sendRequest(`${BASE_URL}/cart/qty`, 'PUT', { productId, newQty }),
-       sendRequest(`${BASE_URL}/cart/qty`, 'PUT', { serviceId, newQty })
-    );
+import sendRequest from './send-request';
+
+const BASE_URL = '/api/orders';
+
+export function getCart() {
+  return sendRequest(`${BASE_URL}/cart`);
 }
-  
+
+export function addProdToCart(productId) {
+  return sendRequest(`${BASE_URL}/cart/products/${productId}`, 'POST');
+}
+
+export function setProdQtyInCart(productId, newQty) {
+  return sendRequest(`${BASE_URL}/cart/qty`, 'PUT', { productId, newQty });
+}
+
 export function checkout() {
-    return sendRequest(`${BASE_URL}/cart/checkout`, 'POST');
+  return sendRequest(`${BASE_URL}/cart/checkout`, 'POST');
 }
-  
+
 export function getOrderHistory() {
-    return sendRequest(`${BASE_URL}/history`);
+  return sendRequest(`${BASE_URL}/history`);
 }
